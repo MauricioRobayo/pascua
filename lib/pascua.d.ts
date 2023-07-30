d69a4619e40750ed1e6e9551b14f9ed89637584c
Mauricio Robayo <rfmajo@gmail.com>
2023-07-30 11:12:16 -0500

    remove lib

--- lib/pascua.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Easter {
-    year: number;
-    month: number;
-    day: number;
-    toString: () => string;
-}
-declare function pascua(year?: number): Easter;
-export default pascua;
