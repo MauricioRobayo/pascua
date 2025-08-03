20b01f02530d199efe9e0407eaa965448dc306b6
Mauricio Robayo <rfmajo@gmail.com>
2025-08-03 10:24:10 -0500

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,15 @@
-interface Easter {
+export interface Easter {
     year: number;
     month: number;
     day: number;
     toString: () => string;
 }
 
-export type { Easter };
+
+/**
+ * Returns the Easter date for a given year (Gregorian calendar).
+ * @param year The year for which to calculate Easter. Defaults to the current year.
+ * @throws Error if the year is not between 1583 and 4099.
+ */
+declare function pascua(year?: number): Easter;
+export default pascua;
